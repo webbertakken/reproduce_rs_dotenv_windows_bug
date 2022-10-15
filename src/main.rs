8e03fb5a12ea506b2ca3bfd8b49501fe6ed9a248
Webber <webber@takken.io>
2022-10-15 14:55:13 +0200

    chore: prevent potential clash

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use dotenv::dotenv;
 fn main() {
     dotenv().ok();
 
+    println!("Logging SOME_VAR if present");
+
     for (key, value) in env::vars() {
-        if key == "RUST_LOG" {
+        if key == "SOME_VAR" {
             println!("{}: {}", key, value);
         }
     }
 
-    println!("logged all envs.");
+    println!("notice SOME_VAR being present in .env but not being logged on Windows unless specified on system level");
 }
